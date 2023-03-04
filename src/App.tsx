fbb11f8483826e1d7c299676a6a09d3e35f7d32e
LiuJB0128 <liujianbing128@gmail.com>
2023-03-04 20:47:35 +0800

    TSX代替Template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { defineComponent, ref } from "vue";
+
+export const App = defineComponent({
+  setup() {
+    const refCount = ref(0)
+    const onClick = () => {refCount.value++}
+    return () => <>
+      <div>
+        {refCount.value}
+      </div>
+      <button onClick={onClick}>+1</button>
+    </>
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn: true, // 自动转换
+      mergeProps: true // 自动帮你把 class属性 / style属性 / onXXX属性 绑定到子组件的根元素上
+    })
+  ]
 })
