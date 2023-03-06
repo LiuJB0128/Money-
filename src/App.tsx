11438f0935abcbc71128df75cc8baa36ebb41fa8
LiuJB0128 <liujianbing128@gmail.com>
2023-03-07 02:11:33 +0800

    修复移动端一开始无法全屏的bug以及禁止用户缩放

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import './App.scss'
 
 export const App = defineComponent({
   setup() {
-    const refCount = ref(0)
-    const onClick = () => {refCount.value++}
     return () => ( 
-      <div>
+      <div class="page">
         <RouterView/>
       </div>
     )
