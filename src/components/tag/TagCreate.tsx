83607d65ebe33b82f82a8a2fe8636ee3b3b23bd1
LiuJB0128 <liujianbing128@gmail.com>
2023-03-09 15:03:56 +0800

    修复emoji无法显示的bug，完成TagCreate页面

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,9 +1,10 @@
 import { Button } from 'vant';
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, reactive } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import SvgIcon from '../svgIcon/index.vue';
 import styles from './TagCreate.module.scss'
 import 'vant/es/button/style'
+import { EmojiSelect } from '../../shared/EmojiSelect';
 
 export const TagCreate = defineComponent({
   props: {
@@ -12,189 +13,45 @@ export const TagCreate = defineComponent({
     }
   },
   setup: (props, context) => {
+    const formData = reactive({
+      name: '',
+      sign: '',
+    })
+    const onSubmit = () => {
+      console.log(formData)
+    }
     return () => (
       <MainLayout>{{
         title: () => '新建标签',
         icon: () => <SvgIcon name="left"></SvgIcon>,
         default: () => (
-          <form class={styles.form}>
-            <div class={styles.formRow}>
-              <label class={styles.formLabel}>
-                <span class={styles.formItem_name}>标签名</span>
-                <div class={styles.formItem_value}>
-                  <input class={[styles.formItem, styles.input, styles.error]}></input>
+          <form class={ styles.form } onSubmit={ onSubmit }>
+            <div class={ styles.formRow }>
+              <label class={ styles.formLabel }>
+                <span class={ styles.formItem_name }>标签名</span>
+                <div class={ styles.formItem_value }>
+                <input v-model={ formData.name } class={ [styles.formItem, styles.input, styles.error] }></input>
                 </div>
-                <div class={styles.formItem_errorHint}>
+                <div class={ styles.formItem_errorHint }>
                   <span>必填</span>
                 </div>
               </label>
             </div>
-            <div class={styles.formRow}>
-              <label class={styles.formLabel}>
-                <span class={styles.formItem_name}>符号</span>
-                <div class={styles.formItem_value}>
-                  <div class={[styles.formItem, styles.emojiList, styles.error]}>
-                    <nav>
-                      <span class={styles.selected}>表情</span>
-                      <span>手势</span>
-                      <span>职业</span>
-                      <span>衣服</span>
-                      <span>动物</span>
-                      <span>自然</span>
-                      <span>食物</span>
-                      <span>运动</span>
-                      <span>表情</span>
-                      <span>手势</span>
-                      <span>职业</span>
-                      <span>衣服</span>
-                      <span>动物</span>
-                      <span>自然</span>
-                      <span>食物</span>
-                      <span>运动</span>
-                    </nav>
-                    <ol>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                      <li>😀</li>
-                    </ol>
-                  </div>
+            <div class={ styles.formRow }>
+              <label class={ styles.formLabel }>
+                <span class={ styles.formItem_name }>符号  { formData.sign }</span>
+                <div class={ styles.formItem_value }>
+                <EmojiSelect  v-model={ formData.sign } class={ [styles.formItem, styles.emojiList, styles.error] } />
                 </div>
-                <div class={styles.formItem_errorHint}>
+                <div class={ styles.formItem_errorHint }>
                   <span>必填</span>
                 </div>
               </label>
             </div>
-            <p class={styles.tips}>记账时长按标签即可进行编辑</p>
-            <div class={styles.formRow}>
-              <div class={styles.formItem_value}>
-                <Button class={[styles.formItem, styles.button]}>确定</Button>
+            <p class={ styles.tips }>记账时长按标签即可进行编辑</p>
+            <div class={ styles.formRow }>
+              <div class={ styles.formItem_value }>
+                <Button class={ [styles.formItem, styles.button] } onClick={onSubmit}>确定</Button>
               </div>
             </div>
           </form>
---
+++ src/shared/EmojiSelect.tsx
@@ -0,0 +1,61 @@
+import { computed, defineComponent, PropType, ref } from 'vue';
+import { emojiList } from './emojiList';
+import styles from './EmojiSelect.module.scss';
+export const EmojiSelect = defineComponent({
+  props: {
+    modelValue: {
+      type: String
+    }
+  },
+  setup: (props, context) => {
+    const refSelected = ref(0)
+    const table: [string, string[]][] = [
+      ['表情', ['face-smiling', 'face-affection', 'face-tongue', 'face-hand',
+        'face-neutral-skeptical', 'face-sleepy', 'face-unwell', 'face-hat',
+        'face-glasses', 'face-concerned', 'face-negative', 'face-costume'
+      ]],
+      ['出行', ['transport-ground', 'transport-water', 'transport-air', 'hotel',
+        'place-building', 'place-geographic'
+      ]],
+      ['手势', ['hand-fingers-open', 'hand-fingers-partial', 'hand-single-finger',
+        'hand-fingers-closed', 'hands', 'hand-prop', 'body-parts']],
+      ['人物', ['person', 'person-gesture', 'person-role', 'person-fantasy',
+        'person-activity', 'person-sport', 'person-resting']],
+      ['衣服', ['clothing']],
+      ['动物', ['cat-face', 'monkey-face', 'animal-mammal', 'animal-bird',
+        'animal-amphibian', 'animal-reptile', 'animal-marine', 'animal-bug']],
+      ['植物', ['plant-flower', 'plant-other']],
+      ['自然', ['sky & weather', 'science']],
+      ['食物', [
+        'food-fruit', 'food-vegetable', 'food-prepared', 'food-asian',
+        'food-marine', 'food-sweet'
+      ]],
+      ['运动', ['sport', 'game']],
+    ]
+    const onClickTab = (index: number) => {
+      refSelected.value = index
+    }
+    const onClickEmoji = (emoji: string) => {
+      context.emit('update:modelValue', emoji)
+    }
+    const emojis = computed(() => {
+      return table[refSelected.value][1].map(category =>
+        emojiList.find(item => item[0] === category)?.[1]
+          .map(item => <li class={ item === props.modelValue ? styles.selectedEmoji : '' }
+            onClick={ () => onClickEmoji(item) }>{ item }</li>)
+      )
+    })
+    return () => (
+      <div class={ styles.emojiList }>
+        <nav>
+          {table.map((item, index) =>
+            <span class={ index === refSelected.value ? styles.selected : '' }
+              onClick={ () => onClickTab(index)}>{item[0]}</span>) }
+        </nav>
+        <ol>
+          { emojis.value }
+        </ol>
+      </div>
+    )
+  }
+})
