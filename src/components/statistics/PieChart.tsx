5b06d4e605a10f5ad6da63cc5491ce3b9dcd1918
LiuJB0128 <liujianbing128@gmail.com>
2023-03-16 01:59:59 +0800

    完善饼图

--- src/components/statistics/PieChart.tsx
+++ src/components/statistics/PieChart.tsx
@@ -1,59 +1,68 @@
-import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { defineComponent, onMounted, PropType, ref, watch } from 'vue';
 import styles from './PieChart.module.scss';
 import * as echarts from 'echarts';
+import { getMoney } from '../../shared/Money'
+const defaultOption = {
+  tooltip: {
+    trigger: 'item',
+    formatter: (x: {name:string, value:number, percent: number})=>{
+      const {name,value,percent} = x
+      return `${name}: ￥${getMoney(value)} 占比 ${percent}%`
+    }
+  },
+  grid: [
+    { left: 0, top: 0, right: 0, bottom: 0 }
+  ],
+  series: [
+    {
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      itemStyle: {
+        borderRadius: 10,
+        borderColor: '#fff',
+        borderWidth: 2
+      },
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 50,
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      
+    }
+  ]
+}
 export const PieChart = defineComponent({
   props: {
-    name: {
-      type: String as PropType<string>
+    data: {
+      type: Array as PropType<{name:string, value: number}[]>
     }
   },
   setup: (props, context) => {
     const refDiv2 = ref<HTMLDivElement>()
+    let chart: echarts.ECharts | undefined = undefined
+      // 绘制图表
     onMounted(() => {
       if (refDiv2.value === undefined) { return }
       // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(refDiv2.value);
-      // 绘制图表
-      const option = {
-        grid: [
-          { left: 0, top: 0, right: 0, bottom: 20 }
-        ],
-        series: [
-          {
-            name: 'Access From',
-            type: 'pie',
-            radius: ['40%', '70%'],
-            avoidLabelOverlap: false,
-            itemStyle: {
-              borderRadius: 10,
-              borderColor: '#fff',
-              borderWidth: 2
-            },
-            label: {
-              show: false,
-              position: 'center'
-            },
-            emphasis: {
-              label: {
-                show: true,
-                fontSize: 40,
-                fontWeight: 'bold'
-              }
-            },
-            labelLine: {
-              show: false
-            },
-            data: [
-              { value: 1048, name: 'Search Engine' },
-              { value: 735, name: 'Direct' },
-              { value: 580, name: 'Email' },
-              { value: 484, name: 'Union Ads' },
-              { value: 300, name: 'Video Ads' }
-            ]
-          }
-        ]
-      };
-      myChart.setOption(option);
+      chart = echarts.init(refDiv2.value);
+      chart.setOption(defaultOption);
+    })
+    watch(() => props.data, () => {
+      chart?.setOption({
+        series: [{
+          data: props.data
+        }]
+      })
     })
     return () => (
       <div ref={refDiv2} class={styles.wrapper}></div>
