e367384075658f85060c75a464003f820f02f75d
LiuJB0128 <liujianbing128@gmail.com>
2023-03-16 00:39:42 +0800

    数据补零

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -5,7 +5,9 @@ import { Bars } from './Bars';
 import { LineChart } from './LineChart';
 import { PieChart } from './PieChart';
 import { http } from '../../shared/Http';
+import { Time } from '../../shared/time';
 
+const DAY = 24 * 3600 * 1000
 type Data1Item = {happen_at:string, amount: number}
 type Data1 = Data1Item[]
 export const Charts = defineComponent({
@@ -22,10 +24,18 @@ export const Charts = defineComponent({
   setup: (props, context) => {
     const kind = ref('expenses')
     const data1 = ref<Data1>([])
-    const betterData1 = computed(()=> {
-      return data1.value.map(item=>
-        [item.happen_at, item.amount] as [string, number]
-      )
+    const betterData1 = computed<[string, number][]>(()=> {
+      if(!props.startDate || !props.endDate) { return [] }
+      const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
+      const n = diff / DAY + 1
+      return Array.from({length: n}).map((_, i)=>{
+        const time = new Time(props.startDate+'T00:00:00.000+0800').add(i, 'day').getTimestamp()
+        const item = data1.value[0]
+        const amount = (item && new Date(item.happen_at).getTime() === time)
+          ? data1.value.shift()!.amount
+          : 0
+        return [new Date(time).toISOString(), amount]
+      })
     })
 
     onMounted(async ()=>{
@@ -35,8 +45,6 @@ export const Charts = defineComponent({
         kind: kind.value,
         _mock: 'itemSummary'
       })
-      console.log('response.data')
-      console.log(response.data)
       data1.value = response.data.groups
     })
     return () => (
