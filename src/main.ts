7cb05c38c85815da1df0ee1271456e3ee2ec32f7
LiuJB0128 <liujianbing128@gmail.com>
2023-03-16 20:24:17 +0800

    引入 Pinia

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { createRouter } from 'vue-router'
 import { routes } from './config/routes'
 import { history } from './shared/history'
 import 'virtual:svg-icons-register'
+import { createPinia } from 'pinia'
 
 const router = createRouter({  history, routes })
 
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
